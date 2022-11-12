4fd9574ad4481dfd1cb2b6faf92f5dd086fec121
znjs <jasrikar@gmail.com>
2022-11-12 17:47:48 +0530

    config: nextjs, tailwindcss, redux-toolkit

---
+++ redux/actions/demoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  number: 10,
+  string: 'Hello Znjs!',
+  loading: false,
+  error: false,
+};
+
+const demoSlice = createSlice({
+  name: 'demo',
+  initialState,
+  reducers: {
+    updateNumber: (state, action) => {
+      state.number = action.payload.number;
+    },
+    updateString: (state, action) => {
+      state.string = action.payload.string;
+    },
+  },
+});
+
+export const demoReducer = demoSlice.reducer;
+export const { updateNumber, updateString } = demoSlice.actions;
